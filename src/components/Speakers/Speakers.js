3870af410dc9c021f6edce2589a54f965a07dca7
enzoesper <enzoesper@hotmail.com>
2020-12-20 14:37:37 -0300

    separating each speaker in an individual image and storing in an array of speakers

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 const Speakers = () => {
+
+  const speakers = [
+    {
+      imageSrc: "speaker-component-1124",
+      name: "Douglas Crockford"
+    },
+    {
+      imageSrc: "speaker-component-1530",
+      name: "Tamara Baker"
+    },
+    {
+      imageSrc: "speaker-component-10803",
+      name: "Eugene Chuvyrov"
+    }
+  ]
+
   return (
     <div>
-      <img src="images/speakers.png" alt="speakers"/>
+      {speakers.map((speaker) => {
+        return <img src={`/images/${speaker.imageSrc}.png`} alt={speaker.name} key={speaker.imageSrc}/>
+      })}
     </div>
   );
 };
