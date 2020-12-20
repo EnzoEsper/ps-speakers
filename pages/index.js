c933e918670df16198cf53e2d60c9796c3cac284
enzoesper <enzoesper@hotmail.com>
2020-12-20 17:31:15 -0300

    adding more assets and finalizing the refactoring and styling of the site using tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-// import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
+import Layout from '../src/components/Layout/Layout';
 
-export default function Home() {
+function Page() {
   return (
-    <div>
-      <h1>Hello from index.js PS</h1>
-    </div>
-  )
+    <Layout>
+      <div className="text-gray-600">
+        <div className="m-6">
+          <p>
+            Silicon Valley Code Camp is a two-day technical conference in
+            Silicon Valley where developers learn from developers focusing on
+            open source, the latest enterprise-focused technologies, software
+            branding, legal issues around software as well as other topics
+            developers are interested in hearing about such as career building
+            and more. topics developers are interested in hearing about.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
 }
+export default Page;
--- pages/speakers.js
+++ pages/speakers.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import Header from "../src/components/Header/Header";
-import Menu from "../src/components/Menu/Menu";
-import SpeakerSearchBar from "../src/components/SpeakerSearchBar/SpeakerSearchBar";
 import Speakers from "../src/components/Speakers/Speakers";
-import Footer from "../src/components/Footer/Footer";
-
-import SpeakerContext from '../src/components/Speakers/SpeakerContext';
+import Layout from '../src/components/Layout/Layout';
 
 export default function Page() {
-  const speakers = [
-    {
-      imageSrc: "speaker-component-1124",
-      name: "Douglas Crockford"
-    },
-    {
-      imageSrc: "speaker-component-1530",
-      name: "Tamara Baker"
-    },
-    {
-      imageSrc: "speaker-component-10803",
-      name: "Eugene Chuvyrov"
-    }
-  ];
   return (
-    <div>
-      <Header />
-      <Menu />
-      <SpeakerContext.Provider value={speakers}>
-        <SpeakerSearchBar />
+      <Layout>
         <Speakers />
-      </SpeakerContext.Provider>
-      <Footer />
-    </div>
+      </Layout>
   )
 }
