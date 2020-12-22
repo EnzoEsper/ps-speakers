67053e5b89bccc7448feb7c02c27956f936e966d
enzoesper <enzoesper@hotmail.com>
2020-12-22 00:28:27 -0300

    showing data loading status and error codes when loading REST data

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -54,28 +54,63 @@ const Speakers = () => {
     const toggledSpeakerRec = toggleSpeakerFavorite(speakerRec);
     const speakerIndex = speakers.map((speaker) => speaker.id).indexOf(speakerRec.id);
 
-    await axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, toggledSpeakerRec);
+    try {
+      await axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, toggledSpeakerRec);
+      setSpeakers([...speakers.slice(0, speakerIndex), toggledSpeakerRec, ...speakers.slice(speakerIndex + 1)]);
+    } catch (error) {
+      setStatus(REQUEST_STATUS.ERROR);
+      setError(error);
+    }
 
-    setSpeakers([...speakers.slice(0, speakerIndex), toggledSpeakerRec, ...speakers.slice(speakerIndex + 1)]);
   }
   
   const [searchQuery, setSearchQuery] = useState("");
   const [speakers, setSpeakers] = useState([]);
 
+  const REQUEST_STATUS = {
+    LOADING: "loading",
+    SUCCESS: "success",
+    ERROR: "error"
+  };
+
+  const [status, setStatus] = useState(REQUEST_STATUS.LOADING);
+  const [error, setError] = useState({});
+
   useEffect(() => {
     
     const fetchData = async() => {
-      const response = await axios.get("http://localhost:4000/speakers");
-      setSpeakers(response.data);
+      try {
+        const response = await axios.get("http://localhost:4000/speakers");
+        setSpeakers(response.data);
+        setStatus(REQUEST_STATUS.SUCCESS);
+      } catch (error) {
+        setStatus(REQUEST_STATUS.ERROR);
+        setError(error);
+      }
     }
 
     fetchData();
-  }, [])
+  }, []);
+
+  const success = status === REQUEST_STATUS.SUCCESS;
+  const isLoading = status === REQUEST_STATUS.LOADING;
+  const hasErrored = status === REQUEST_STATUS.ERROR;
 
   return (
     <div>
       <SpeakerSearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
+
+      {isLoading && <div>Loading...</div>}
+
+      {hasErrored && (
+        <div> 
+          Loading error... Is the json-server running? (try "npm run json-server" at terminal prompt)
+          <br/>
+          <b>ERROR: {error.message}</b>
+        </div>
+      )}
+
+      {success && <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
         {speakers
         .filter((rec) => {
           const targetString = `${rec.firstName} ${rec.lastName}`.toLowerCase();
@@ -84,7 +119,7 @@ const Speakers = () => {
         .map((speaker) => (
           <Speaker key={speaker.id} {...speaker} onFavoriteToggle={() => onFavoriteToggleHandler(speaker)}/>
         ))}
-      </div>
+      </div>}
     </div>
   );
 };
